00afec8f77f11209e3ea2c71c887de99eacb7e58
RamiroVT <u202123714@upc.edu.pe>
2024-06-23 02:49:42 -0500

    add: docker config

--- auth-service/src/test/java/org/arquitech/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/org/arquitech/authservice/AuthServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package org.arquitech.authservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceApplicationTests {
 
     @Test
